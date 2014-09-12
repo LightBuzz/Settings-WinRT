8d0df72a916dd44af70b8208300d99f216aeb16f
Vangos <pterneas@gmail.com>
2014-09-12 22:34:00 +0300

    Added Roaming settings support

---
+++ LightBuzz.Settings/LightBuzz.Settings/ApplicationDataExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace LightBuzz.Settings
+{
+    /// <summary>
+    /// Provides some extension methods for setting, getting and deleting application data.
+    /// </summary>
+    public static class ApplicationDataExtensions
+    {
+        /// <summary>
+        /// Inserts the specified value to the specified settings key, or creates a new pair.
+        /// </summary>
+        /// <typeparam name="T">A primitive type.</typeparam>
+        /// <param name="key">The name of the setting.</param>
+        /// <param name="value">The content of the setting.</param>
+        public static void Set<T>(this ApplicationDataContainer container, string key, T value)
+        {
+            container.Values[key] = value;
+        }
+
+        /// <summary>
+        /// Retrieves the contents of the specified setting key.
+        /// </summary>
+        /// <typeparam name="T">A primitive type.</typeparam>
+        /// <param name="key">The name of the setting.</param>
+        /// <returns>The content of the spcified setting.</returns>
+        public static T Get<T>(this ApplicationDataContainer container, string key)
+        {
+            if (container.Values.Keys.Contains(key))
+            {
+                return (T)container.Values[key];
+            }
+
+            T value = default(T);
+
+            container.Set<T>(key, value);
+
+            return value;
+        }
+
+        /// <summary>
+        /// Deletes the specified setting.
+        /// </summary>
+        /// <param name="key">The name of the setting.</param>
+        public static void Remove(this ApplicationDataContainer container, string key)
+        {
+            container.Values.Remove(key);
+        }
+    }
+}
--- LightBuzz.Settings/LightBuzz.Settings/Settings.cs
+++ LightBuzz.Settings/LightBuzz.Settings/Settings.cs
@@ -1,33 +1,4 @@
-﻿// Copyright (c) LightBuzz, Inc.
-// All rights reserved.
-//
-// http://lightbuzz.com
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions
-// are met:
-//
-// 1. Redistributions of source code must retain the above copyright
-//    notice, this list of conditions and the following disclaimer.
-//
-// 2. Redistributions in binary form must reproduce the above copyright
-//    notice, this list of conditions and the following disclaimer in the
-//    documentation and/or other materials provided with the distribution.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
-// OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
-// AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
-// WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-// POSSIBILITY OF SUCH DAMAGE.
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -36,46 +7,25 @@ using Windows.Storage;
 
 namespace LightBuzz.Settings
 {
+    /// <summary>
+    /// Provides access to the application settings.
+    /// </summary>
     public static class Settings
     {
         /// <summary>
-        /// Inserts the specified value to the specified settings key, or creates a new pair.
+        /// Gets the local application settings.
         /// </summary>
-        /// <typeparam name="T">A primitive type.</typeparam>
-        /// <param name="key">The name of the setting.</param>
-        /// <param name="value">The content of the setting.</param>
-        public static void Set<T>(string key, T value)
+        public static ApplicationDataContainer Local
         {
-            ApplicationData.Current.LocalSettings.Values[key] = value;
+            get { return ApplicationData.Current.LocalSettings; }
         }
 
         /// <summary>
-        /// Retrieves the contents of the specified setting key.
+        /// Gets the roaming application settings.
         /// </summary>
-        /// <typeparam name="T">A primitive type.</typeparam>
-        /// <param name="key">The name of the setting.</param>
-        /// <returns>The content of the spcified setting.</returns>
-        public static T Get<T>(string key)
+        public static ApplicationDataContainer Roaming
         {
-            if (ApplicationData.Current.LocalSettings.Values.Keys.Contains(key))
-            {
-                return (T)ApplicationData.Current.LocalSettings.Values[key];
-            }
-
-            T value = default(T);
-
-            Set<T>(key, value);
-
-            return value;
-        }
-
-        /// <summary>
-        /// Deletes the specified setting.
-        /// </summary>
-        /// <param name="key">The name of the setting.</param>
-        public static void Remove(string key)
-        {
-            ApplicationData.Current.LocalSettings.Values.Remove(key);
+            get { return ApplicationData.Current.RoamingSettings; }
         }
     }
 }
